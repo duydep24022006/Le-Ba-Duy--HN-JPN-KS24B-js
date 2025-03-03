43cc766413d1b54d32104cdcbf9b757ed1d1c0ba
duydep24022006 <memay26032006@gmail.com>
2025-03-03 21:49:49 +0700

    bai 19

---
+++ ss19/baitap2.js
@@ -0,0 +1,14 @@
+
+let inputChar = prompt("Nhập một ký tự:");
+let doDai = inputChar.length;
+if(doDai>1){
+    if(inputChar.indexOf(0&&1&&2&&3&&4&&5&&6&&7&&8&&9)== -1){
+        console.log(inputChar);
+        document.write("khong phai la mot chu");
+    }else{
+        document.write("la mot chu");
+    }
+}else{
+    document.write("khong phai chu");
+}
+
