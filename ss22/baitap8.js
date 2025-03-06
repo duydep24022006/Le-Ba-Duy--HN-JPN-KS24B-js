09ab8798b94a5d51c80d491095157e3bdd78d19e
duydep24022006 <memay26032006@gmail.com>
2025-03-06 20:41:53 +0700

    bai 22

--- ss22/baitap8.js
+++ ss22/baitap8.js
@@ -1,10 +1,27 @@
-let arr = [2, 5, 2, 3, 7, 23, 1, 6, 0, 4];
-let count = 0;
+let testCases = [
 
-if (Number.isInteger(input)) {
-    for (let i = 0; i < arr.length; i++) {
-        if (input == arr[i]) {
-            count++;
+    [3, 9, 7, 4, 1, 8, 6, 2, 5, 7]
+];
+
+for (let arr of testCases) {
+    let freq = new Map();
+    let maxFreq = 0;
+    let minValue = Infinity;
+
+    for (let num of arr) {
+        if (freq.has(num)) {
+            freq.set(num, freq.get(num) + 1);
+        } else {
+            freq.set(num, 1);
+        }
+        maxFreq = Math.max(maxFreq, freq.get(num));
+    }
+
+    for (let [key, value] of freq) {
+        if (value === maxFreq) {
+            minValue = Math.min(minValue, key);
         }
     }
+
+    console.log("Output:", minValue);
 }
